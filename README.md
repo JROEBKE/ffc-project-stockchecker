d703503ad526de8d96c08077232978fe79c8d3f7
Jonas Roebke <getintouchwithjonas@gmail.com>
2020-04-26 15:06:20 +0200

    stock data get works as wished

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Project Stock Price Checker
 
 User Stories
 Set the content security policies to only allow loading of scripts and css from your server. //DONE
-I can GET /api/stock-prices with form data containing a Nasdaq stock ticker and recieve back an object stockData. //TODO
-In stockData, I can see the stock(string, the ticker), price(decimal in string format), and likes(int). //TODO
+I can GET /api/stock-prices with form data containing a Nasdaq stock ticker and recieve back an object stockData. //DONE
+In stockData, I can see the stock(string, the ticker), price(decimal in string format), and likes(int). //DONE
 I can also pass along field like as true(boolean) to have my like added to the stock(s). Only 1 like per ip should be accepted. //TODO
 If I pass along 2 stocks, the return object will be an array with both stock's info but instead of likes, it will display rel_likes(the difference between the likes on both) on both. //TODO
 A good way to receive current prices is through our stock price proxy (replacing 'GOOG' with your stock symbol): https://repeated-alpaca.glitch.me/v1/stock/GOOG/quote //TODO
--- routes/api.js
+++ routes/api.js
@@ -9,13 +9,87 @@
 'use strict';
 
 
+//adding validator
+const { check, validationResult } = require('express-validator');
+
+const axios = require('axios');
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res){[
+      //TODO validation matters
+      ];
+      var like = req.query.like;
+      console.log(like);
+      var stock = req.query.stock;
 
-      // https://repeated-alpaca.glitch.me/v1/stock/GOOG/quote
+      // check if stock is single entry = string or an object
+      if (typeof stock ==='string') {
+        console.log('single stock entry');
+        var stockData = await axios.get('https://repeated-alpaca.glitch.me/v1/stock/'+stock+'/quote')
+        .then(function (response) {
+          var output ={};
+          output.stock = response.data.symbol;
+          output.price = response.data.latestPrice;
+
+          //TODO add helper function to check for IP and addition we must store this probably
+          if(like){
+            output.likes = 1;
+          } else {
+            output.likes = 0;
+          }
+
+          return output;            
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+      } else {
+        var stockData =[];
+        for(var i=0; i<stock.length; i++){        
+          await axios.get('https://repeated-alpaca.glitch.me/v1/stock/'+stock[i]+'/quote')
+          .then(function (response) {
+            var output ={};
+            output.stock = response.data.symbol;
+            output.price = response.data.latestPrice;
+            console.log(output);
+            stockData.push(output);
+
+
+            //TODO add helper function to check for IP and addition we must store this probably
+            if(like){
+              output.likes = 1;
+            } else {
+              output.likes = 0;
+            }
+
+            return;            
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+        };
+      }
+
+
+      var result = stockData;
+
+
+      //TODO add rel_likes to result
+      
+      return res.status(200).json({
+          stockData: result});
+
+      //{"stockData":{"stock":"GOOG","price":1279.31,"likes":1}}
+      //{"stockData":[{"stock":"GOOG","price":1279.31}]}
+
+      //{"stockData":[{"stock":"GOOG","price":1279.31,"rel_likes":0},{"stock":"MSFT","price":174.55,"rel_likes":0}]}
+      //{"stockData":[{"stock":"GOOG","price":1279.31},{"stock":"MSFT","price":174.55}]}
+
+      
     });
     
 };
